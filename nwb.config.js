ca635d66c1618a14f19455146bd7414f928b8f51
Jack Reed <phillipjreed@gmail.com>
2019-05-30 16:14:52 -0600

    Initial commit from nwb v0.23.0

---
+++ nwb.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'react-component',
+  npm: {
+    esModules: true,
+    umd: {
+      global: 'MiradorDownload',
+      externals: {
+        react: 'React'
+      }
+    }
+  }
+}
