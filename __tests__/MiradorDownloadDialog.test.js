fc9a1b5fa2ad1a4965370b4c05d0d7cd4cda52b1
Jessie Keck <jessie.keck@gmail.com>
2020-01-29 16:41:23 -0800

    Pass canvasIndex to the selectInfoResponse selector (so it knows which infoResponse to return)

--- __tests__/MiradorDownloadDialog.test.js
+++ __tests__/MiradorDownloadDialog.test.js
@@ -12,6 +12,7 @@ function createWrapper(props) {
       classes={{}}
       closeDialog={() => {}}
       containerId="container-123"
+      infoResponse={() => ({})}
       manifest={{ getSequences: () => [] }}
       open
       viewType="single"
@@ -64,6 +65,84 @@ describe('Dialog', () => {
       });
 
       expect(wrapper.find('ManifestDownloadLinks').length).toBe(1);
+    });
+  });
+});
+
+describe('mapStateToProps', () => {
+  const state = {
+    infoResponses: {
+      'https://example.com/image/iiif/abc123_0001': {
+        json: {
+          width: 2579,
+          height: 3638,
+          sizes: [
+            {
+              width: 81,
+              height: 114,
+            },
+            {
+              width: 161,
+              height: 227,
+            },
+            {
+              width: 322,
+              height: 455,
+            },
+            {
+              width: 645,
+              height: 909,
+            },
+            {
+              width: 1290,
+              height: 1819,
+            },
+            {
+              width: 2579,
+              height: 3638,
+            },
+          ],
+        },
+      },
+    },
+    manifests: {
+      'http://example.com/abc123/iiif/manifest': {
+        json: {
+          '@type': 'sc:Manifest',
+          sequences: [
+            {
+              canvases: [
+                {
+                  images: [
+                    {
+                      resource: {
+                        service: {
+                          '@id': 'https://example.com/image/iiif/abc123_0001',
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+    windows: {
+      'window-abc123': {
+        manifestId: 'http://example.com/abc123/iiif/manifest',
+      },
+    },
+    windowDialogs: {},
+    config: {},
+  };
+  const props = { windowId: 'window-abc123' };
+  const mapStateToProps = miradorDownloadDialog.mapStateToProps(state, props);
+
+  describe('infoResponse', () => {
+    it('gets the correct info response from state', () => {
+      expect(mapStateToProps.infoResponse(0).json.sizes.length).toBe(6);
     });
   });
 });
